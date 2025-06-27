59ce50e3c55bf5adc2b6d926d72ca293154e4768
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2025-06-27 17:40:19 +0700

    Update words_en.js

--- public/lang/words_en.js
+++ public/lang/words_en.js
@@ -1,33 +1,38 @@
 var WORDS = [
-	// Animals
-	"animal|tiger", "animal|crocodile", "animal|chicken", "animal|spider", "animal|butterfly",
-	"animal|frog", "animal|rabbit", "animal|elephant", "animal|squirrel", "animal|chameleon",
-	"animal|giraffe", "animal|horse", "animal|lion", "animal|penguin", "animal|octopus",
-
-	// Fruits
-	"fruit|apple", "fruit|watermelon", "fruit|avocado", "fruit|cherry", "fruit|chocolate",
-	"fruit|coconut", "fruit|grapefruit", "fruit|strawberry", "fruit|banana", "fruit|pineapple", "fruit|kiwi",
-
-	// Sports
-	"sport|football", "sport|basketball", "sport|badminton", "sport|swimming", "sport|baseball",
-	"sport|bowling", "sport|karate", "sport|wrestling", "sport|surfing", "sport|tennis", "sport|volleyball",
-
-	// Colors
-	"color|red", "color|blue", "color|green", "color|yellow", "color|purple", "color|pink", "color|orange", "color|black", "color|white", "color|brown",
-
-	// Countries
-	"country|Romania", "country|France", "country|Germany", "country|Japan", "country|Brazil",
-	"country|India", "country|Canada", "country|Australia", "country|Italy", "country|Spain",
-
-	// Movies
-	"movie|Titanic", "movie|Avatar", "movie|Inception", "movie|Frozen", "movie|Minions", "movie|Shrek", "movie|Spiderman", "movie|Batman", "movie|Harry Potter",
-
-	// Jobs
-	"job|doctor", "job|teacher", "job|pilot", "job|engineer", "job|chef", "job|police officer", "job|firefighter", "job|dentist", "job|actor", "job|youtuber",
-
-	// Objects
-	"object|phone", "object|computer", "object|bottle", "object|chair", "object|table", "object|camera", "object|pencil", "object|backpack", "object|lamp",
-
-	// Emotions
-	"emotion|happy", "emotion|sad", "emotion|angry", "emotion|surprised", "emotion|scared", "emotion|excited", "emotion|bored"
-];
+	"Hewan Apakah?|kadal",
+	"Hewan Apakah?|tikus",
+	"Hewan Apakah?|ayam",
+	"Hewan Apakah?|kambing",
+	"Hewan Apakah?|keong",
+	"Hewan Apakah?|komodo",
+	"Hewan Apakah?|landak",
+	"Hewan Apakah?|gajah",
+	"Hewan Apakah?|domba",
+	"Hewan Apakah?|ular",
+	"Hewan Apakah?|bebek",
+	"Hewan Apakah?|bunglon",
+	"Hewan Apakah?|capung",
+	"Hewan Apakah?|harimau",
+	"Hewan Apakah?|kuda",
+	"Hewan Apakah?|sapi",
+	"Hewan Apakah?|ikan",
+	"Buah Apakah?|kedondong",
+	"Buah Apakah?|apel",
+	"Buah Apakah?|alpukat",
+	"Buah Apakah?|cherry",
+	"Buah Apakah?|pisang",
+	"Buah Apakah?|jeruk",
+	"Buah Apakah?|nangka",
+	"Buah Apakah?|melon",
+	"Buah Apakah?|strawberry",
+	"Buah Apakah?|pepaya",
+	"Buah Apakah?|manggis",
+	"Buah Apakah?|markisa",
+	"Buah Apakah?|melon",
+	"Buah Apakah?|mangga",
+	"Buah Apakah?|durian",
+	"Buah Apakah?|anggur",
+	"Buah Apakah?|kurma",
+	"Buah Apakah?|jambu",
+	"Buah Apakah?|malaka"
+]
